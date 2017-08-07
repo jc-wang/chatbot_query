620132a9825e04a9f831ae7080c8eef9c4d68a01
antonio <tgq.spm@gmail.com>
2017-08-07 16:23:14 +0200

    Administrative setting.

--- install.sh
+++ install.sh
@@ -2,14 +2,8 @@
 # Installing
 sudo pip3 install -r requirements.txt
 sudo python3 setup.py install
+sudo python3 -m textblob.download_corpora
 # Deleting trash files
 sudo rm -r build/ || true
 sudo rm -r dist/ || true
 sudo rm -r chatbotQuery.egg-info/ || true
-#if  [ -d "$build"]; then
-#    sudo rm -r build/
-#fi
-#if  [ -d "$dist"]; then
-#    sudo rm -r dist/
-#    sudo rm -r chatbotQuery.egg-info/
-#fi
