6cc930b17b8c6767cd42077929b98486eba2188a
antonio <tgq.spm@gmail.com>
2017-08-07 12:37:22 +0200

    Adapt example to new functionalities.

--- example_dbquery.py
+++ example_dbquery.py
@@ -18,10 +18,10 @@ import numpy as np
 
 from chatbot.conversation import TalkingState, SequentialChooser,\
     TransitionConversationStates, ConversationStateMachine, QuerierState,\
-    QuerierSplitterChooser, CheckerState, StoringState
+    QuerierSplitterChooser, CheckerState, StoringState, QuestioningState
 
 from chatbot.aux_functions import create_fixed_condition, splitter,\
-    yes_no_answer
+    yes_no_answer, create_yes_no_question
 from chatbot.ui import HandlerConvesationDB, TerminalUIHandler
 from chatbot.dbapi import DataBaseAPI
 
@@ -35,7 +35,7 @@ QUESTIONS_SALUT_DB =\
      "Do you want me to find something? Which products?",
      "What are the products you are interested in?"]
 QUESTIONS_QUERY_DB =\
-    [{'message': 'I have not find anything for you.', 'level_query': 4},
+    [{'message': "I haven't find anything for you.", 'level_query': 4},
      {'message': 'Are you searching for products {query_catnames}?',
       'level_query': 3},
      {'message': 'We have: {query_productnames}', 'level_query': 2},
@@ -56,6 +56,10 @@ QUESTIONS_MORE = ['Do you want something more?',
                   'Do you have any other question?',
                   'Anything else?']
 QUESTIONS_REASKER = ['Be more specific.']
+QUESTION_EXTRA_Q =\
+    ["""Do you want to keep exploring that options?
+Or there is between these products the one you are searching for."""]
+QUESTION_EXTRA_PRODUCTS = ["Which products?"]
 
 QUESTIONS_END_EXPLORE = ['Do you want to keep exploring that options?']
 
@@ -65,7 +69,10 @@ QUESTIONS_GOODBYE = ["It's been a pleasure. See you next time!",
                      "Have a nice day!"]
 
 
-### Auxiliar functions
+###############################################################################
+###############################################################################
+### Auxiliar functions to define dbapi response formatter
+### Definition of responses
 def category_joiner(l):
     if len(l) == 0:
         return ''
@@ -91,11 +98,59 @@ def joiner_categories(code, l):
     return s
 
 
+def price_q_keywords(keywords, pre):
+    from stemming.porter2 import stem
+    from textblob import TextBlob
+    words = ['price', 'cost']
+    if type(keywords) == list:
+        keywords = copy.copy(keywords[0])
+    text = TextBlob(keywords)
+    logi = any([word in [stem(w) for w in text.stripped.split()]
+                for word in words])
+    return logi
+
+
+###############################################################################
+
+
+###############################################################################
+###############################################################################
+### Auxiliar functions to define dbapi response formatter
+### Definition of responses
 def checker_cats_sure(h, m):
+    ## 'Ensure categories' state definition `querier`
+    return m
+
+
+def check_storer_products(h, m):
+    ## 'Explore products' state definition `querier`
+    q = h.query_past_queries()
+    h.store_query(q[0])
+    return m
+
+
+def querier_prices_f(h, m):
+    ## 'Shows prices' state definition `querier`
+    last_query = handler_db.query_past_queries()
+    ids = last_query[0]['query']['query_idxs']
+    query_info = h.databases['db'].get_query_info_from_ids(ids,
+                                                           label=True)
+    return query_info
+
+
+def storer_query(h, m):
+    ## 'Store categories' state definition 'querier'
+#    m2 = h.query_past_queries(1)[0]
+    m2 = copy.copy(m)
+    m2['query']['query_names']['main_var'] = []
+    m2['query']['query_idxs']['main_var'] =\
+        np.array([np.array([], dtype=np.int64)])
+    h.store_query(m2)
     return m
 
 
 def price_q(response):
+    ## TODO: pass into DBAPI: DONE
     from stemming.porter2 import stem
     from textblob import TextBlob
     words = ['price', 'cost']
@@ -105,20 +160,72 @@ def price_q(response):
     return int(logi)
 
 
-def check_storer_products(h, m):
-    q = h.query_past_queries()
-    h.store_query(q[0])
-    return m
+def cond_queries_db(message):
+    ## 'Explore products' state definition `transition`.
+    ## Dummy selection to keep exploring in that way or not
+    ## The good is to insert a classifier
+    ## 0. Query with price and products
+    ## 1. Only yes answer
+    ## 2. No
+    f_no = create_yes_no_question('yes')
+    f_price = lambda x: price_q_keywords(x, None)
+    m = message['message']
+    ms = [e.strip() for e in m.replace('.', ' ').split(' ')]
+    if (len(ms) > 10):
+        i = 0
+    else:
+        if f_price(m):
+            i = 0
+        if not f_no(message):
+            i = 0
+            if len(ms) == 1:
+                i = 1
+        else:
+            i = 2
+    return i
 
 
-def querier_prices_f(h, m):
-        last_query = handler_db.query_past_queries()
-        ids = last_query[0]['query']['query_idxs']
-        query_info = h.databases['db'].get_query_info_from_ids(ids,
-                                                               label=True)
-        return query_info
+############################ Querier DB definition ############################
+def querier_f(h, m):
+    ## 'Querier DB' state definition `querier`.
+#    m = h.query_past_messages(sender='user', tag='query')[0]['message']
+#    keywords = get_keywords(m)
+    pre_query = None
+    if len(h.queriesDB):
+        pre_query = h.queriesDB[-1]['query']
+    if 'query' in m:
+        pre_query = m['query']
+    query_info =\
+        h.databases['db'].get_query_info([m['message']], pre_query)
+    return query_info
 
 
+def cond_selector(query_info):
+    ## 'Querier DB' state definition `chooser` function
+    i = splitter(len(query_info['main_var'][0]), [0, .1, 1, 5, 20000])
+    if i == 3:
+        if all([len(v[0]) == 0 for c, v in query_info['cat_vars'].items()]):
+            i = 4
+#        for c, v in query_info['cat_vars'].items():
+#            if len(v) != 0:
+#                break
+#        i = 4
+    return i
+
+
+def cond_collapser_trans(message):
+    ## 'Querier DB' state definition `chooser` function
+    i = message['level_query']
+    collapsing_info = [[0], [1, 2], [3], [4]]
+    for j in range(len(collapsing_info)):
+        if i in collapsing_info[j]:
+            break
+    else:
+        raise Exception("Not in collapsing info.")
+    return j
+
+
+############################### Store categories ##############################
 if __name__ == "__main__":
 
     # Parse data
@@ -127,8 +234,11 @@ if __name__ == "__main__":
     assert(isfile(args[1]))
     datapath = args[1]
 
+    ############################ Query conversation ###########################
+    ## Defining query conversation
     states = []
-    ### Conversation querier
+
+    ## Defining 'Hello DB' state
     q_hello_db = [{'message': m} for m in QUESTIONS_SALUT_DB]
     cond_hellodb = create_fixed_condition(0)
     trans_states = ['Querier DB']
@@ -138,66 +248,39 @@ if __name__ == "__main__":
                                tags=['query'], transition=trans_hellodb,
                                shadow=False))
 
-    def querier_f(h, m):
-#        m = h.query_past_messages(sender='user', tag='query')[0]['message']
-#        keywords = get_keywords(m)
-        pre_query = None
-        if len(h.queriesDB):
-            pre_query = h.queriesDB[0]['query']['query_idxs']
-        query_info =\
-            h.databases['db'].get_query_info([m['message']], pre_query)
-        return query_info
-
-    def cond_selector(query_info):
-        i = splitter(len(query_info['main_var'][0]), [0, .1, 1, 5, 20000])
-        if i == 3:
-            for c, v in query_info['cat_vars'].items():
-                if len(v) != 0:
-                    break
-            else:
-                i = 4
-        return i
-
-    def cond_collapser_trans(message):
-        i = message['level_query']
-        collapsing_info = [[0, 4], [1, 2], [3]]
-        for j in range(len(collapsing_info)):
-            if i in collapsing_info[j]:
-                break
-        else:
-            raise Exception("Not in collapsing info.")
-        return j
-
+    ## Defining 'Querier DB' state
     chooser_query = QuerierSplitterChooser(QUESTIONS_QUERY_DB, 'level_query',
                                            cond_selector,
                                            query_var='query_idxs')
-    trans_states = ['Keep Trying', 'Explore products', 'Ensure categories']
+    trans_states = ['Keep Trying', 'Explore products',
+                    'Explore extra products', 'Closing Check']
     query_trans = TransitionConversationStates(trans_states,
                                                cond_collapser_trans)
     states.append(QuerierState('Querier DB', querier_f, chooser_query,
-                               tags=['query'], transition=query_trans))
+                               tags=['query'], transition=query_trans,
+                               shadow=True))
 
-    ############################ Storing categories ###########################
     ## Defining 'Ensure categories' state
-    trans_states = ['Querier DB', 'Store categories']
-    trans_cats_sure = TransitionConversationStates(trans_states, yes_no_answer)
-    states.append(CheckerState('Ensure categories', checker_cats_sure,
-                               transition=trans_cats_sure))
+    q_null = [{'message': ''}]
+    chooser_null = SequentialChooser(q_null)
+    trans_states = ['Querier DB']
+    cond_trans = create_fixed_condition(0)
+    trans_cats_sure = TransitionConversationStates(trans_states, cond_trans)
+#    states.append(CheckerState('Ensure categories', checker_cats_sure,
+#                               transition=trans_cats_sure))
+    states.append(QuestioningState('Ensure categories',
+                                   chooser_null,
+                                   transition=trans_cats_sure,
+                                   shadow=False))
 
-    def storer_query(h, m):
-#        m2 = h.query_past_queries(1)[0]
-        m2 = copy.copy(m)
-        m2['query']['query_names']['main_var'] = []
-        m2['query']['query_idxs']['main_var'] =\
-            np.array([np.array([], dtype=np.int64)])
-        h.store_query(m2)
-        return m
+    ## Defining 'Store categories' state
     cond_trans = create_fixed_condition(0)
     trans_states = ['ReAsker']
     trans_storename = TransitionConversationStates(trans_states, cond_trans)
     states.append(StoringState('Store categories', storer_query,
                                trans_storename))
 
+    ## Defining 'ReAsker' state
     ## Define a yes_no final asker
     q_reasker = [{'message': m} for m in QUESTIONS_REASKER]
     cond_trans = create_fixed_condition(0)
@@ -207,12 +290,38 @@ if __name__ == "__main__":
     states.append(TalkingState('ReAsker', chooser_end,
                                transition=trans_reask))
 
-    ############################# Storing Products ############################
     ## Defining 'Explore products' state
-    trans_states = ['Keep Trying', 'Show Prices']
-    trans_prices = TransitionConversationStates(trans_states, price_q)
-    states.append(CheckerState('Explore products', check_storer_products,
-                               transition=trans_prices))
+    q_explore_products = [{'message': m} for m in QUESTION_EXTRA_Q]
+    chooser_explore_products = SequentialChooser(q_explore_products)
+    trans_states = ['Querier DB', 'Explore extra products', 'Closing Check']
+    trans_explore = TransitionConversationStates(trans_states, cond_queries_db)
+#    states.append(CheckerState('Explore products', check_storer_products,
+#                               transition=trans_explore))
+    states.append(QuestioningState('Explore products',
+                                   chooser_explore_products,
+                                   transition=trans_explore,
+                                   shadow=False))
+
+#    ## Defining 'Reset Query' state
+#    # Download questions
+#    q_null = [{'message': ''}]
+#    chooser_null = SequentialChooser(q_null)
+#    trans_states = ['Closing Check']
+#    cond_trans = create_fixed_condition(0)
+#    trans_null = TransitionConversationStates(trans_states, cond_trans)
+#    states.append(QuestioningState('Reset Query', chooser_null,
+#                                   transition=trans_null, shadow=True))
+
+    ## Defining 'Explore extra products' state
+    q_explore_xtra_products = [{'message': m} for m in QUESTION_EXTRA_PRODUCTS]
+    chooser_xtra_explore_products = SequentialChooser(q_explore_xtra_products)
+    cond_trans = create_fixed_condition(0)
+    trans_states = ['Querier DB']
+    trans_explore = TransitionConversationStates(trans_states, cond_trans)
+    states.append(QuestioningState('Explore extra products',
+                                   chooser_xtra_explore_products,
+                                   transition=trans_explore,
+                                   shadow=False))
 
     ## Defining 'Show Prices' state
     cond_trans = create_fixed_condition(0)
@@ -224,7 +333,7 @@ if __name__ == "__main__":
     states.append(QuerierState('Show Prices', querier_prices_f, chooser_query,
                                tags=['query'], transition=query_trans))
 
-    ################################# Goodbye #################################
+    ## Defining 'Keep Trying' state
     ## Define a yes_no keep trying
     q_end_explore = [{'message': m} for m in QUESTIONS_END_EXPLORE]
     trans_states = ['Closing Check', 'Querier DB']
@@ -234,11 +343,26 @@ if __name__ == "__main__":
                                transition=trans_askmore))
 
     ## Defining 'Closing Check' state
-#    closing_check = lambda h, m: print(m);m
+#    closing_check = lambda h, m: m
     def closing_check(h, m):
-        return h.query_past_messages(sender='user')[0]
+        ## Clean queries
+        h.queriesDB.append({'query': None})
+        m['query'] = None
+#        return h.query_past_messages(sender='user')[0]
+        return m
     states.append(CheckerState('Closing Check', closing_check))
 
+    ############################ Query conversation
+    ### Conversation Query
+    cond_query = create_fixed_condition(0)
+    trans_states = ['Final Asker']
+    trans_query = TransitionConversationStates(trans_states, cond_query)
+
+    query_conv = ConversationStateMachine('Query Conversation', states,
+                                          'Hello DB', ['Closing Check'],
+                                          transition=trans_query)
+
+    ############################ Hello conversation ###########################
     ### Hello
     q_hello = [{'message': m} for m in HELLO_SENTENCES]
     cond_hello = create_fixed_condition(0)
@@ -249,6 +373,8 @@ if __name__ == "__main__":
                                shadow=True)
     hello_conv = ConversationStateMachine('Hello', [hello_state], 'Hello',
                                           ['Hello'], transition=trans_hello)
+
+    ########################## Final Ask conversation #########################
     ## Define a yes_no final asker
     q_end_sure = [{'message': m} for m in QUESTIONS_END_SURE]
     trans_states = ['Say goodbye', 'Query Conversation']
@@ -260,21 +386,16 @@ if __name__ == "__main__":
     final_ask_conv = ConversationStateMachine('Final Asker', [final_ask],
                                               'Final Asker', ['Final Asker'],
                                               transition=trans_askcont)
+
+    ######################### Say Goodbye conversation ########################
     ## Say goodbye
     q_goodbye = [{'message': m} for m in QUESTIONS_GOODBYE]
     state_goodbye = TalkingState('Say goodbye', SequentialChooser(q_goodbye))
     goodbye_conv = ConversationStateMachine('Say goodbye', [state_goodbye],
                                             'Say goodbye', ['Say goodbye'])
 
-    ### Conversation Query
-    trans_states = ['Final Asker', 'Say goodbye']
-    trans_query = TransitionConversationStates(trans_states, yes_no_answer)
-    query_conv = ConversationStateMachine('Query Conversation', states,
-                                          'Hello DB', ['Closing Check'],
-                                          transition=trans_query)
-
     ############################### Conversation ##############################
-
+    ## Aggregation of the conversation
     end_states = ['Say goodbye']
     conv_states = [hello_conv, query_conv, final_ask_conv, goodbye_conv]
     conversa = ConversationStateMachine('Conversation', conv_states,
@@ -289,16 +410,19 @@ if __name__ == "__main__":
                       'codename': ['brand', 'category']},
          'label_var': {'name': 'Subscription Plan'}}
     responses_formatter =\
-        {'main_var': (lambda l: ''.join(['\n    * '+s for s in l]),
+        {'main_var': (lambda l: ''.join(['\n'+' '*8+'* '+s for s in l]),
                       'query_productnames'),
          'cat_vars': {'Brand': joiner_brands, 'Category': joiner_categories},
-         'label_var': (lambda l, p: ''.join(['\n    * '+r+': '+s+"€"
+         'label_var': (lambda l, p: ''.join(['\n'+' '*8+'* '+r+': '+s+"€/month"
                                              for r, s in zip(l, p)]),
                        'query_productnames'),
          'join_cats': (category_joiner, 'query_catnames')
          }
+    parameter_formatter =\
+        {'label': price_q_keywords}
 
-    db_api = DataBaseAPI(datapath, type_vars, responses_formatter)
+    db_api = DataBaseAPI(datapath, type_vars, responses_formatter,
+                         parameter_formatter)
     handler_db = HandlerConvesationDB(databases=db_api)
 
     ## Running a conversation
