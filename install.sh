fbeff6c76a450464da9ee905404c81e1e407f285
antonio <tgq.spm@gmail.com>
2017-08-07 15:28:06 +0200

    Administrative setting.

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo python3 setup.py install
 # Deleting trash files
 if  [ -d "$build"]; then
     sudo rm -r build/
+fi
+if  [ -d "$dist"]; then
     sudo rm -r dist/
     sudo rm -r chatbotQuery.egg-info/
 fi
