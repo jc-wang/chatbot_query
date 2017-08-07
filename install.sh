c94258aed55c3ab8474e613848c623cf7d0927f1
antonio <tgq.spm@gmail.com>
2017-08-07 16:20:01 +0200

    Administrative setting.

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 sudo pip3 install -r requirements.txt
 sudo python3 setup.py install
 # Deleting trash files
-if  [ -d "$build"]; then
-    sudo rm -r build/
-fi
-if  [ -d "$dist"]; then
-    sudo rm -r dist/
-    sudo rm -r chatbotQuery.egg-info/
-fi
+sudo rm -r build/ || true
+sudo rm -r dist/ || true
+sudo rm -r chatbotQuery.egg-info/ || true
+#if  [ -d "$build"]; then
+#    sudo rm -r build/
+#fi
+#if  [ -d "$dist"]; then
+#    sudo rm -r dist/
+#    sudo rm -r chatbotQuery.egg-info/
+#fi
