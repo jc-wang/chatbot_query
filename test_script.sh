8a2ba93d291de695b74a8b0cb9e896c2eedf3c94
antonio <tgq.spm@gmail.com>
2017-10-13 18:26:07 +0200

    test script

--- test_script.sh
+++ test_script.sh
@@ -1,2 +1,2 @@
-python3 -m "nose" --with-coverage --cover-package=chatbotQuery
+python3 -m "nose" --where chatbotQuery/tests --with-coverage --cover-package=chatbotQuery
 sudo ./clean_package.sh || true
